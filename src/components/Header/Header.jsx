572db3fc0d7e179815686d8fca55c7a584a09173
kristiyan675 <vasilev.kristiyan.v@gmail.com>
2024-06-03 17:19:07 +0300

    bug fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Menu } from "antd";
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 const AppHeader = () => {
+  // fix antd UI bug with current active link
+  const location = useLocation();
+  const [current, setCurrent] = useState();
+
+  useEffect(() => {
+    if (location.pathname === "/") {
+      setCurrent("1");
+    } else if (location.pathname !== "/note/new") {
+      setCurrent(null);
+    }
+  }, [location.pathname]);
+
+  function handleClick(e) {
+    setCurrent(e.key);
+  }
+
   const menuItems = [
     {
       key: "1",
@@ -19,12 +37,23 @@ const AppHeader = () => {
       <div
         style={{ color: "white", fontSize: "24px", fontWeight: "bold" }}
         className="navText"
+        onClick={(e) => {
+          if (current !== "1") {
+            handleClick(e);
+          }
+        }}
       >
         <Link to="/" style={{ color: "white" }}>
           Notes App
         </Link>
       </div>
-      <Menu theme="dark" mode="horizontal" items={menuItems} />
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        items={menuItems}
+        selectedKeys={[current]}
+        onClick={handleClick}
+      />
     </>
   );
 };
